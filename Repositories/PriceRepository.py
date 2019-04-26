d06b759be026f5d187b0e32fd156e8110a665542
SSA\danil.kovalenko <D.Kovalenko@total.one>
2019-04-26 17:16:28 +0300

    [added] - pushing pricing to db; init db;

--- Repositories/PriceRepository.py
+++ Repositories/PriceRepository.py
@@ -3,41 +3,49 @@ import pymysql
 from Helpers.MappingHelper import to_dynamic
 from Helpers.SettingsHelper import settings
 
-connection = pymysql.connect(host=settings['connectionInfo']['host'],
-                                     user=settings['connectionInfo']['user'],
-                                     password=settings['connectionInfo']['password'],
-                                     db=settings['connectionInfo']['database'],
-                                     charset=settings['connectionInfo']['charset'])
 
 def import_from_socket(json_string):
-    print('IMPORTED')
     print(json_string)
-    try:
-        with connection.cursor() as cursor:
-            # sql = """CREATE TABLE `test` (
-            #         `id` int(11) NOT NULL AUTO_INCREMENT,
-            #         `email` varchar(500) COLLATE utf8_bin NOT NULL,
-            #         `password` varchar(500) COLLATE utf8_bin NOT NULL,
-            #         PRIMARY KEY (`id`)
-            #         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
-            #         AUTO_INCREMENT=1 ;
-            #         """
-            # cursor.execute(sql)
+    connection = pymysql.connect(host=settings['connectionInfo']['host'],
+                                 user=settings['connectionInfo']['user'],
+                                 password=settings['connectionInfo']['password'],
+                                 db=settings['connectionInfo']['database'],
+                                 charset=settings['connectionInfo']['charset'])
 
+    try:
+        pricingModel = to_dynamic(json_string).data
+
+        with connection.cursor() as cursor:
             # Create a new record
-            sql = "INSERT INTO `test` (`email`, `password`) VALUES (%s, %s)"
-            cursor.execute(sql, (to_dynamic(json_string).type, 'very-secret'))
+            sql = "INSERT INTO `PricingTable` (" \
+                  "`i`," \
+                  "`m`," \
+                  "`ask`," \
+                  "`ask_volume`," \
+                  "`bid`," \
+                  "`bid_volume`," \
+                  "`close`," \
+                  "`high`," \
+                  "`last`," \
+                  "`last_volume`," \
+                  "`low`," \
+                  "`open`," \
+                  "`tick_timestamp`," \
+                  "`trade_timestamp`," \
+                  "`turnover`," \
+                  "`turnover_volume`," \
+                  "`vwap`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+            print(sql)
+            cursor.execute(sql, (
+                pricingModel.i, pricingModel.m, pricingModel.ask, pricingModel.ask_volume,
+                pricingModel.bid, pricingModel.bid, pricingModel.bid_volume,
+                pricingModel.close, pricingModel.high, pricingModel.last, pricingModel.last_volume, pricingModel.low,
+                pricingModel.open, pricingModel.tick_timestamp,
+                pricingModel.trade_timestamp, pricingModel.turnover, pricingModel.turnover_volume, pricingModel.vwap))
 
         # connection is not autocommit by default. So you must commit to save
         # your changes.
         connection.commit()
-
-        with connection.cursor() as cursor:
-            # Read a single record
-            sql = "SELECT `id`, `email`, `password` FROM `test` WHERE `password`=%s"
-            cursor.execute(sql, ('very-secret',))
-            result = cursor.fetchone()
-            print(result)
     finally:
-        #connection.close()
-        print('INSERTED!')
+        connection.close()
